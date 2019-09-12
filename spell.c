0330a9d334e3624c75aefbf443c924824e39429b
Gary Johnson <garycjohnso@gmail.com>
2019-09-11 22:03:09 -0400

    Add reference comments based on professors update

--- spell.c
+++ spell.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 
+// PROF REPO: https://github.com/kcg295/AppSecAssignment1
+// words separated by spaces
+// strip punctuation at the end only
+// punctuation in the middle is misspelled
+// "" etc at the beginning should be removed
+
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]) {
 
   char buf[LENGTH];
@@ -32,13 +38,11 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
   if(strlen(dictionary_file) < 1) {
     return false;
   }
-  if(sizeof(hashtable) / sizeof(hashmap_t) < HASH_SIZE) {
-    return false;
-  }
 
   fp = fopen(dictionary_file, "r");
   if (fp != NULL) {
     while((rtn_value = fgets(buf, LENGTH+1, fp)) != NULL) {
+      printf("%s\n", buf);
       bucket_value = hash_function(buf);
       if((tmp = hashtable[bucket_value]) == NULL) {
         if((tmp = malloc(sizeof(node))) != NULL) {
@@ -49,9 +53,11 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
           return false;
         }
       } else {
+        printf("tmp word: %s\ntmp next: %s\n", tmp->word, tmp->next);
         while(tmp->next != NULL) {
           tmp = tmp->next;
         }
+        printf("C\n");
         if((tmp2 = malloc(sizeof(node)))!= NULL) {
           strncpy(tmp2->word, buf, sizeof(buf));
           tmp2->next = NULL;
@@ -87,28 +93,13 @@ bool check_word(const char* word, hashmap_t hashtable[]) {
 
   return false;
 }
-// int main(int argc, char **argv) {
-//
-//   int i;
-//   char *wordlist = argv[2];
-//   char *inputlist = argv[1];
-//   char *misspelled;
-//   hashmap_t hashtable[HASH_SIZE];
-//   hashmap_t tmp;
-//
-//   for (i = 0; i < HASH_SIZE; i++) {
-//     tmp = malloc(sizeof(node));
-//     if(tmp != NULL) {
-//       tmp->word[0] = '\0';
-//       tmp->next = NULL;
-//       hashtable[i] = tmp;
-//     } else {
-//       printf("Failed to allocate memory");
-//       exit(0);
-//     }
-//   }
-//
-//   load_dictionary(wordlist, hashtable);
-//   // check_words(fp, hashtable, misspelled);
-//   return 0;
-// }
+int main(int argc, char **argv) {
+
+  char *wordlist = argv[2];
+  char *inputlist = argv[1];
+  hashmap_t hashtable[HASH_SIZE];
+
+  load_dictionary(wordlist, hashtable);
+  // check_words(fp, hashtable, misspelled);
+  return 0;
+}
