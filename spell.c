46fc4d6963d7c87ccd1e60295179e9eff8ad8a84
tes <tes>
2019-09-20 23:47:26 -0400

    Remove main

--- spell.c
+++ spell.c
@@ -171,23 +171,23 @@ bool check_word(const char* word, hashmap_t hashtable[]) {
 
   return false;
 }
-int main(int argc, char **argv) {
-
-  char *wordlist = argv[2];
-  char *text = argv[1];
-  char *misspelled[MAX_MISSPELLED];
-  FILE *fp;
-  int i = 0;
-
-  hashmap_t hashtable[HASH_SIZE];
-  fp = fopen(text, "r");
-
-  load_dictionary(wordlist, hashtable);
-  printf("Found %d bad words\n", check_words(fp, hashtable, misspelled));
-
-  for(i = 0; i<5; i++) {
-    printf("%s\n", misspelled[i]);
-    i++;
-  }
-  return 0;
-}
+// int main(int argc, char **argv) {
+//
+//   char *wordlist = argv[2];
+//   char *text = argv[1];
+//   char *misspelled[MAX_MISSPELLED];
+//   FILE *fp;
+//   int i = 0;
+//
+//   hashmap_t hashtable[HASH_SIZE];
+//   fp = fopen(text, "r");
+//
+//   load_dictionary(wordlist, hashtable);
+//   printf("Found %d bad words\n", check_words(fp, hashtable, misspelled));
+//
+//   for(i = 0; i<5; i++) {
+//     printf("%s\n", misspelled[i]);
+//     i++;
+//   }
+//   return 0;
+// }
